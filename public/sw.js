16aa5d0b8c3da3874c3fefb057ec14d3a6eb7d73
guyrofe-create <guyrofe@gmail.com>
2025-08-17 15:06:11 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -90,4 +90,6 @@ self.addEventListener('message', (e) => {
     self.registration.showNotification(d.title || 'תזכורת', { body: d.body || '' });
   }
   if (d.type === 'SKIP_WAITING') self.skipWaiting();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
